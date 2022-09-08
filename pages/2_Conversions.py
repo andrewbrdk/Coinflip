ca68aa6f63a1b66c944bee25f393e0cbe7198503
andrew <andrew.brdk@gmail.com>
2022-09-08 11:44:34 +0300

    Remove duration for max expected convs

--- pages/2_Conversions.py
+++ pages/2_Conversions.py
@@ -73,9 +73,6 @@ def pb_ge_pa_sims(s_a, s_b, n_cmp=30000):
     pb = stats.beta.rvs(s_b['alpha_post'], s_b['beta_post'], size=(n_cmp, n_pars))
     return np.sum(pb >= pa, axis=0) / n_cmp   
 
-def expected_conv_after_choice(pa_mean, pb_mean, pb_ge_pa, n_rest):
-    return int(pa_mean * (1 - pb_ge_pa) * n_rest + pb_mean * pb_ge_pa * n_rest)
-
 def simulate(p, trials, alpha, beta):
     trials_conv = stats.binom.rvs(n=trials, p=p)
     trials_accum = np.cumsum(trials)
@@ -91,20 +88,12 @@ def simulate(p, trials, alpha, beta):
     }
     return s 
 
-def min_n_to_reach_certainty_level(probs_pb_ge_pa, trials_accum, required_pb_ge_pa):
+def min_days_to_reach_certainty_level(probs_pb_ge_pa, days, required_pb_ge_pa):
     prob_gt_required = (probs_pb_ge_pa > required_pb_ge_pa) | (probs_pb_ge_pa < 1 - required_pb_ge_pa)
-    reached = trials_accum[prob_gt_required]
-    min_reached = np.min(reached) if prob_gt_required[-1] else np.max(trials_accum)
+    reached = days[prob_gt_required]
+    min_reached = np.min(reached) if prob_gt_required[-1] else np.max(days)
     return min_reached
-
-def conversions_on_choice(a_sim, b_sim, probs_pb_ge_pa, trials_accum, n_total_affected):
-    pa_mean = stats.beta(a_sim['alpha_post'], a_sim['beta_post']).mean()
-    pb_mean = stats.beta(b_sim['alpha_post'], b_sim['beta_post']).mean()
-    after_choice = np.array([expected_conv_after_choice(pa_m, pb_m, pb_ge_pa, n_total_affected - n_exp) 
-                              for pa_m, pb_m, pb_ge_pa, n_exp
-                              in zip(pa_mean, pb_mean, probs_pb_ge_pa, trials_accum)])
-    sim_and_expected_convs = after_choice + a_sim['trials_conv_accum'] + b_sim['trials_conv_accum']
-    return sim_and_expected_convs  
+ 
 
 def init_conv_session_values():
     if 'conv_a_exact' not in st.session_state:
@@ -119,17 +108,13 @@ def init_conv_session_values():
         st.session_state['conv_b_split'] = 50.0
     if 'conv_pb_gt_pa_required' not in st.session_state:
         st.session_state['conv_pb_gt_pa_required'] = 95.0
-    if 'conv_n_total_affected' not in st.session_state:
-        st.session_state['conv_n_total_affected'] = 10**6
-    if 'conv_sim_max' not in st.session_state:
-        st.session_state['conv_sim_max'] = 100000
-    if 'conv_sim_step' not in st.session_state:
-        st.session_state['conv_sim_step'] = 5000
+    if 'conv_sim_max_days' not in st.session_state:
+        st.session_state['conv_sim_max_days'] = 30
     if 'conv_n_simulations' not in st.session_state:
         st.session_state['conv_n_simulations'] = 100
         
 init_conv_session_values()
-st.session_state
+#st.session_state
 
 st.title('Conversions Comparison')
 
@@ -378,41 +363,31 @@ summary_bar.empty()
 summary_container.write(df_formatted.T, width=600)
 
 
-st.subheader("Duration Estimation")
+st.subheader("Duration Estimates")
 
 col1, col2 = st.columns(2)
 with col1: 
-    st.number_input(label='Max N in Simulations',
-                    min_value=5000,
-                    step=5000,
+    st.number_input(label='Max Days in Simulations',
+                    min_value=1,
+                    step=1,
                     format='%d',
-                    key='conv_sim_max')
+                    key='conv_sim_max_days')
 with col2: 
-    st.number_input(label='N Step in Simulations',
-                    min_value=1000,
-                    step=1000,
+    st.number_input(label='Simulations',
+                    min_value=1,
+                    step=1,
                     format='%d',
-                    key='conv_sim_step')
+                    key='conv_n_simulations')
 
-n_simulations = st.number_input(label='Simulations',
-                                min_value=1,
-                                step=1,
-                                format='%d',
-                                key='conv_n_simulations')
-n_sim_steps = st.session_state['conv_sim_max'] // st.session_state['conv_sim_step']
+st.number_input(label='Required P(p_B > p_A)',
+                min_value=0.0,
+                step=1.0,
+                format='%f',
+                key='conv_pb_gt_pa_required')
 
-pb_gt_pa_required = st.number_input(label='Required P(p_B > p_A)',
-                                    min_value=0.0,
-                                    step=1.0,
-                                    format='%f',
-                                    key='conv_pb_gt_pa_required')
-pb_gt_pa_required = pb_gt_pa_required / 100
+n_simulations = st.session_state['conv_n_simulations']
+pb_gt_pa_required = st.session_state['conv_pb_gt_pa_required'] / 100
 
-st.number_input(label='Total Affected Users',
-                min_value=1000,
-                step=1000,
-                format='%d',
-                key='conv_n_total_affected')
 
 
 
@@ -441,7 +416,8 @@ b_post = stats.beta(b_alpha_post, b_beta_post)
 a_p_sim = a_post.rvs(n_simulations)
 b_p_sim = b_post.rvs(n_simulations)
 
-trials = np.append(0, np.full(fill_value=st.session_state['conv_sim_step'], shape=n_sim_steps))
+trials = np.append(0, np.full(fill_value=st.session_state['conv_daily_users'],
+                              shape=st.session_state['conv_sim_max_days']))
 a_trials = np.rint(trials * (1 - conv_b_split)).astype(int)
 b_trials = np.rint(trials * conv_b_split).astype(int)
 
@@ -454,101 +430,49 @@ with st.spinner(text=f'Running {n_simulations} simulations ...'):
         s['A'] = simulate(a_p, a_trials, a_alpha_post, a_beta_post)
         s['B'] = simulate(b_p, b_trials, b_alpha_post, b_beta_post)
         s['pb_ge_pa'] = pb_ge_pa_sims(s['A'], s['B'], n_cmp=10000)
+        s['days'] = np.arange(st.session_state['conv_sim_max_days'] + 1) + st.session_state['conv_n_days'] - 1
         s['N'] = s['A']['trials_accum'] + s['B']['trials_accum']
         s['pb_gt_pa_required'] = st.session_state['conv_pb_gt_pa_required'] / 100
-        s['min_n_to_reach_certainty_lvl'] = min_n_to_reach_certainty_level(s['pb_ge_pa'], s['N'], s['pb_gt_pa_required'])
-        s['n_total_affected'] = st.session_state['conv_n_total_affected']
-        s['expected_conversions_on_choice'] = conversions_on_choice(s['A'], s['B'], s['pb_ge_pa'], s['N'], s['n_total_affected'])
-        s['n_for_max_expected_conv'] = s['N'][np.argmax(s['expected_conversions_on_choice'])]
+        s['min_days_to_reach_certainty_lvl'] = min_days_to_reach_certainty_level(s['pb_ge_pa'], s['days'], s['pb_gt_pa_required'])
         sims.append(s)
         i = i + 1
         my_bar.progress(i / n_simulations)
-        #todo: update spinner text if i % 10 == 0: st.spinner(text=f'finished {i} of {n_simulations} simulations')
 my_bar.empty()
 
-n_reached_hist = [s['min_n_to_reach_certainty_lvl'] for s in sims]
-n_max_hist = [s['n_for_max_expected_conv'] for s in sims]
+n_reached_hist = [s['min_days_to_reach_certainty_lvl'] for s in sims]
+n_reached_freqs = pd.Series(n_reached_hist).value_counts(normalize=True).rename('freq').to_frame()
+x_med = np.median(n_reached_hist)
+if len(n_reached_freqs['freq']) == 1:
+    summary_line = f"100% simulations reached certainty at day {n_reached_freqs.index[0]}"
+else:
+    summary_line = f"50% simulations reached {pb_gt_pa_required*100:.0f}% certainty at day {x_med:.0f} or earlier"
+
 summary_container.write(f"""
-    Estimated experiment duration to reach P(p_experiment > p_base) = {st.session_state['conv_pb_gt_pa_required']}: {np.mean(n_reached_hist)}  
-    Duration for maximum expected conversions: {np.mean(n_max_hist)}  
+    Required certainty: {st.session_state['conv_pb_gt_pa_required']}%  
+    {summary_line}
 """)
 
-
-n_fact = widedf['n_users_accum']['A'] + widedf['n_users_accum']['B']
-pb_ge_pa_fact = widedf['pb_gt_pa']
-
-fig = make_subplots(rows=2, cols=1, shared_xaxes=True)#, 
-                    #column_widths=[0.5, 0.5],
-                    #subplot_titles=("N for Required Certainty", "Simulations"))
-#fig = go.Figure()
-fig.add_trace(go.Histogram(x=n_reached_hist + n_fact.iloc[-1],
-                           histnorm='probability',
-                           name='N to required P(p_b >= p_a) certainty', marker_color='red',
-                           opacity=0.6),
-              row=1, col=1)
-fig.add_vline(x=np.mean(n_reached_hist) + n_fact.iloc[-1], line_dash='dash', row=1, col=1)
-fig.update_layout(title=f'N to reach P(p_b >= p_a) = {pb_gt_pa_required * 100} or P(p_a >= p_b) = {pb_gt_pa_required * 100} %',
-                  xaxis_title='N',
-                  yaxis_title='% from total simulations',
-                  barmode='overlay')
-fig.update_xaxes(range=[0, st.session_state['conv_sim_max'] + 1])
-#st.plotly_chart(fig)
-
-#fig = go.Figure()
-
-fig.add_trace(go.Scatter(x=n_fact, y=pb_ge_pa_fact,
-                         mode='lines', line_color='blue',
-                         hovertemplate=f"Fact, N:%x, p:%y"),
-              row=2, col=1)
-for s in sims:
-    col = 'red' #if (s['pb_ge_pa'][-1] > pb_gt_pa_required) or (s['pb_ge_pa'][-1] < 1 - pb_gt_pa_required) else 'blue'
-    fig.add_trace(go.Scatter(x=s['N'] + n_fact.iloc[-1],
-                             y=s['pb_ge_pa'],
-                             mode='lines', line_color=col, opacity=0.2,
-                             hovertemplate=f"a_p: {s['A']['p'] * 100:.1f} %, b_p = {s['B']['p'] * 100:.1f} %"),
-                  row=2, col=1)
-fig.add_hline(y=pb_gt_pa_required, line_dash='dash', row=2, col=1)
-fig.add_hline(y=1 - pb_gt_pa_required, line_dash='dash', row=2, col=1)
-fig.update_layout(title='N for Required Certainty',
-                  xaxis_title='N',
-                  yaxis_title='P(p_b >= p_a)',
-                  height=550,
+fig = go.Figure()
+fig.add_trace(go.Bar(x=n_reached_freqs.index,
+                     y=n_reached_freqs['freq'],
+                     width=[1] * len(n_reached_freqs),
+                     marker_color='red',
+                     opacity=0.6,
+                     name='Simulations Reached Certainty'))
+fig.add_trace(go.Scatter(x=[x_med, x_med], y=[0, np.max(n_reached_freqs['freq'])], 
+                         line_color='black',
+                         line_dash='dash',
+                         mode='lines',
+                         hovertemplate=f"Median: {x_med}",
+                         name='Median'))              
+fig.update_layout(title=f'Days to Reach {pb_gt_pa_required*100:.0f}% Certainty')
+fig.update_layout(xaxis_title='Days',
+                  yaxis_title='Part from Total Simulations',
                   showlegend=False)
+fig.update_xaxes(range=[0, st.session_state['conv_n_days'] + st.session_state['conv_sim_max_days'] + 1])
+fig.update_layout(yaxis_rangemode='tozero')
 st.plotly_chart(fig)
-#st.write(f'Expected $N$ to reach $P(p_B \ge p_A)$ or $P(p_A \ge p_B) = {pb_gt_pa_required *100}$%: ${np.mean(n_reached_hist)}$')
-
-
-
-#st.write(f'Expected N to reach max ExpConv: {np.mean(n_max_hist)}')
-#probs_at_nmax = [s['pb_ge_pa'][np.argmax(s['sim_and_expected_convs'])] for s in conv_sims]
-
-fig = go.Figure()
-fig.add_trace(go.Histogram(x=n_max_hist, histnorm='probability', 
-                           name='N to required max ExpConv', marker_color='red',
-                           opacity=0.6))
-fig.add_vline(x=np.mean(n_max_hist), line_dash='dash')
-fig.update_layout(title=f'N to reach max ExpConv',
-                  xaxis_title='N',
-                  yaxis_title='% from total simulations',
-                  barmode='overlay')
-st.plotly_chart(fig)
-
-fig = go.Figure()
-for s in sims:
-    col = 'red'
-    fig.add_trace(go.Scatter(x=s['N'] + n_fact.iloc[-1],
-                             y=s['expected_conversions_on_choice'],
-                             mode='lines', line_color=col, opacity=0.2,
-                             hovertemplate=f"a_p: {s['A']['p'] * 100:.1f} %, b_p = {s['B']['p'] * 100:.1f} %"))
-fig.update_layout(title='Simulations',
-                  xaxis_title='N',
-                  yaxis_title='Expected Conversions',
-                  showlegend=False,
-                  height=550)
-st.plotly_chart(fig)
-
-
 
 st.markdown('---')
-st.write("Get the sources at https://github.com/noooway/Coinflip")
-st.write("See theory explanation at https://github.com/noooway/Bayesian_ab_testing")
+st.write("Sources: https://github.com/noooway/Coinflip")
+st.write("Theory: https://github.com/noooway/Bayesian_ab_testing")
